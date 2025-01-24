69c332fc3244454c0824c8534b08309f6462971a
Thomas Laferrière <thomas.laferriere@mda.space>
2025-01-23 22:10:17 -0500

    Flesh out the unsigned parsing

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
 
-const DeserializeError = error{
-    BufferTooLong,
-};
+const DeserializeError = error{ IntTooSmall, BadCast };
 
 pub const Message = struct {
     allocator: std.mem.Allocator,
@@ -24,13 +22,49 @@ pub const Message = struct {
 
     pub fn unpack_as(self: Message, comptime As: type) !As {
         return switch (@typeInfo(As)) {
-            .Int => |int_info| if (int_info.bits <= 64)
-                if (self.buffer.len <= 8)
-                    std.mem.readVarInt(As, self.buffer, std.builtin.Endian.big)
-                else
-                    DeserializeError.BufferTooLong
-            else
-                @compileError("Integer too large for msgpack. Maximum integer size is 64 bits."),
+            .Int => |int_info| switch (int_info.signedness) {
+                .unsigned => switch (self.buffer[0]) {
+                    0xcf => if (int_info.bits >= 64) std.mem.readVarInt(
+                        As,
+                        self.buffer[1..9],
+                        std.builtin.Endian.big,
+                    ) else DeserializeError.IntTooSmall,
+
+                    0xce => if (int_info.bits >= 32) std.mem.readVarInt(
+                        As,
+                        self.buffer[1..5],
+                        std.builtin.Endian.big,
+                    ) else DeserializeError.IntTooSmall,
+
+                    0xcd => if (int_info.bits >= 16) std.mem.readVarInt(
+                        As,
+                        self.buffer[1..3],
+                        std.builtin.Endian.big,
+                    ) else DeserializeError.IntTooSmall,
+
+                    0xcc => if (int_info.bits >= 8)
+                        @intCast(self.buffer[1])
+                    else
+                        DeserializeError.IntTooSmall,
+
+                    else => if (self.buffer[0] & 0x80 == 0)
+                        @intCast(self.buffer[0]) // Unsafe if compiler-optimized.
+                    else
+                        DeserializeError.BadCast,
+                },
+                .signed => switch (self.buffer[0]) {
+                    0xd1, 0xd2, 0xd3 => if (int_info.bits > 8) std.mem.readInt(
+                        As,
+                        self.buffer[1..],
+                        std.builtin.Endian.big,
+                    ),
+                    0xd0 => @intCast(self.buffer[1]),
+                    else => if (self.buffer[0] & 0xE0 == 0xE0)
+                        @intCast(self.buffer[0]) // Unsafe if compiler-optimized.
+                    else
+                        DeserializeError.BadCast,
+                },
+            },
             else => @compileError("Msgpack cannot serialize this type."),
         };
     }
@@ -49,32 +83,65 @@ pub fn pack(allocator: std.mem.Allocator, object: anytype) []const u8 {
     return buffer;
 }
 
-test "Deserialize u8" {
-    const message = try Message.init(testing.allocator, "\x7F");
+test "Deserialize u7" {
+    const message = try Message.init(
+        testing.allocator,
+        "\x7F",
+    );
     defer message.deinit();
-    try testing.expect(try message.unpack_as(u9) == 0x7F);
+    try testing.expect(try message.unpack_as(u7) == 0x7F);
+}
+
+test "Deserialize u8" {
+    const message = try Message.init(
+        testing.allocator,
+        "\xcc\xEF",
+    );
+    defer message.deinit();
+    try testing.expect(try message.unpack_as(u8) == 0xEF);
+}
+
+test "Deserialize u16" {
+    const message = try Message.init(
+        testing.allocator,
+        "\xcd\xBE\xEF",
+    );
+    defer message.deinit();
+    try testing.expect(try message.unpack_as(u16) == 0xBEEF);
 }
 
 test "Deserialize u32" {
-    const message = try Message.init(testing.allocator, "\xDE\xAD\xBE\xEF");
+    const message = try Message.init(
+        testing.allocator,
+        "\xce\xDE\xAD\xBE\xEF",
+    );
     defer message.deinit();
     try testing.expect(try message.unpack_as(u32) == 0xDEADBEEF);
 }
 
 test "Deserialize u64" {
-    const message = try Message.init(testing.allocator, "\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF");
+    const message = try Message.init(
+        testing.allocator,
+        "\xcf\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF",
+    );
     defer message.deinit();
     try testing.expect(try message.unpack_as(u64) == 0xDEADBEEFDEADBEEF);
 }
 
 test "Deserialize u56" {
-    const message = try Message.init(testing.allocator, "\xBE\xEF\xDE\xAD\xBE\xEF");
+    const message = try Message.init(
+        testing.allocator,
+        "\xcf\x00\x00\xBE\xEF\xDE\xAD\xBE\xEF",
+    );
     defer message.deinit();
     try testing.expect(try message.unpack_as(u56) == 0xBEEFDEADBEEF);
 }
 
 test "Deserialize non 8-bit aligned type" {
-    const message = try Message.init(testing.allocator, "\x01\xBE\xEF\xDE\xAD\xBE\xEF");
+    const message = try Message.init(
+        testing.allocator,
+        "\xcf\x00\x01\xBE\xEF\xDE\xAD\xBE\xEF",
+    );
     defer message.deinit();
     try testing.expect(try message.unpack_as(u57) == 0x1BEEFDEADBEEF);
 }
@@ -86,10 +153,14 @@ test "Deserialize non 8-bit aligned type" {
 //     try testing.expect(deserialize_as(u65) == DeserializeError.IntTooLarge);
 // }
 
-test "Deserialize BufferTooLong" {
-    const message = try Message.init(testing.allocator, "\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF");
+test "Deserialize IntTooSmall" {
+    const message = try Message.init(
+        testing.allocator,
+        "\xcf\xDE\xAD\xBE\xEF\xDE\xAD\xBE\xEF",
+    );
     defer message.deinit();
-    try testing.expect(message.unpack_as(u64) == DeserializeError.BufferTooLong);
+    const res = message.unpack_as(u56);
+    try testing.expect(res == DeserializeError.IntTooSmall);
 }
 
 // Compilation error, uncomment to test.
